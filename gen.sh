d28bd1e231e756e999469fe6c1533772f25ef471
Ivan Trofimov <trofimoviv@Mac-mini-Ivan.local>
2020-06-10 17:52:40 +0300

    изменил gen.sh

--- gen.sh
+++ gen.sh
@@ -1,12 +1,12 @@
 git clone https://github.com/kekcik/PSB_CommonProduct_gen &> /dev/null 
 wait
 cd PSB_CommonProduct_gen
-
 npm i ncp replace-in-file readline mkdirp &> /dev/null
 wait
 node main.js
 wait
 cd ..
-cp -R PSB_CommonProduct_gen/Result/. .
+rm -rf Result
+cp -R PSB_CommonProduct_gen/. .
 rm -rf PSB_CommonProduct_gen
 open Result
